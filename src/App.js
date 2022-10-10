e4306053a2260d41cfad297af491821bc2b5a716
jare <adejaremarvel@gmail.com>
2022-10-10 19:57:18 +0100

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import Register from "./Pages/Register";
-
+import HomePage from "./Pages/Homepage";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import PrivateRoutes from "./Authenticator/Auth";
+import Navbar from "./Components/Navbar";
+import Portfolio from "./Pages/Portfolio";
 function App() {
   return (
-    <Register />
+    <Router>
+    <Navbar />
+      <Routes>
+      
+        <Route element = {<PrivateRoutes />}>
+        <Route path='/portfolio' element={<Portfolio/>}/>
+         <Route path='/' element={<HomePage/>} />
+         
+        </Route>
+
+        <Route path = '/register' element= {<Register/>}/>
+      </Routes>
+    </Router>
   );
 }
 
